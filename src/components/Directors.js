0aa6c0c627687b7caf5fb10195483db8e120fe4a
github username <charliepbarth@gmail.com>
2021-03-11 14:00:25 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,27 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const renderDirectors = () => {
+    return directors.map(dir => {
+      return (
+        <div key={dir.name}>
+          <p>{dir.name}</p>
+          <ul>
+            {renderMovies(dir.movies)}
+          </ul>
+        </div>
+      )
+    })
+  }
+
+  const renderMovies = movies => {
+    return movies.map(movie => <li key={movie}>{movie}</li>)
+  }
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {renderDirectors()}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,28 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  const renderMovies = () => {
+    return movies.map(movie => {
+      return (
+        <div key={movie.title}>
+          <p>{movie.title}</p>
+          <p>{movie.time}</p>
+          <ul>  
+            {renderGenres(movie.genres)}
+          </ul>
+        </div>
+      )
+    })
+  }
+
+  const renderGenres = genres => {
+    return genres.map(genre => <li key="genre">{genre}</li>)
+  }
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {renderMovies()}
     </div>
   );
 };
